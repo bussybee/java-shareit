d49126e7c91b103ac1822d1f3f8673468dccf3ba
bussybee <katena.maslova.03@mail.ru>
2024-07-08 21:20:09 +0300

    fix

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -5,7 +5,6 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.error.NotFoundException;
-import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -4,9 +4,6 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
