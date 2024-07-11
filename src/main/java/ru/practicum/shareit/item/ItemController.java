d16b5b530f1959ab12a01ed12a3f4733d5325107
bussybee <katena.maslova.03@mail.ru>
2024-07-11 11:15:18 +0300

    fix: assign an access modifier

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -22,31 +22,31 @@ public class ItemController {
     ItemService itemService;
 
     @PostMapping
-    ResponseEntity<ItemDto> create(@RequestBody @Valid ItemDto item, @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ResponseEntity<ItemDto> create(@RequestBody @Valid ItemDto item, @RequestHeader("X-Sharer-User-Id") Long userId) {
         log.info("Creating new item: {}", item);
         return new ResponseEntity<>(itemService.create(item, userId), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
-    ResponseEntity<ItemDto> update(@RequestBody @Valid ItemUpdateRequestDto item, @PathVariable Long id, @RequestHeader("X-Sharer-User-Id") long userId) {
+    public ResponseEntity<ItemDto> update(@RequestBody @Valid ItemUpdateRequestDto item, @PathVariable Long id, @RequestHeader("X-Sharer-User-Id") long userId) {
         log.info("Updating existing item: {}", item);
         return new ResponseEntity<>(itemService.update(item, id, userId), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<ItemDto> getById(@PathVariable Long id, @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ResponseEntity<ItemDto> getById(@PathVariable Long id, @RequestHeader("X-Sharer-User-Id") Long userId) {
         log.info("Getting item by id: {}", id);
         return new ResponseEntity<>(itemService.getById(id, userId), HttpStatus.OK);
     }
 
     @GetMapping
-    ResponseEntity<List<ItemDto>> getAll(@RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ResponseEntity<List<ItemDto>> getAll(@RequestHeader("X-Sharer-User-Id") Long userId) {
         log.info("Getting all items");
         return new ResponseEntity<>(itemService.getAll(userId), HttpStatus.OK);
     }
 
     @GetMapping("/search")
-    ResponseEntity<List<ItemDto>> searchByText(@RequestParam String text, @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ResponseEntity<List<ItemDto>> searchByText(@RequestParam String text, @RequestHeader("X-Sharer-User-Id") Long userId) {
         log.info("Searching for items by text: {}", text);
         return new ResponseEntity<>(itemService.searchByText(text, userId), HttpStatus.OK);
     }
