2e3bb3f3b0da9c174e375a419de125ac4b000e72
bussybee <katena.maslova.03@mail.ru>
2024-08-11 14:00:09 +0300

    fix

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -6,11 +6,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    //сортировка по какому значению?
     List<Booking> findAllByBooker_IdOrderByStart(Long bookerId);
+
     List<Booking> findAllByBooker_IdAndEndIsBeforeOrderByStart(Long bookerId, LocalDateTime end);
+
     List<Booking> findAllByBooker_IdAndEndIsAfterOrderByStart(Long bookerId, LocalDateTime end);
+
     List<Booking> findAllByBooker_IdAndStartIsAfterOrderByStart(Long bookerId, LocalDateTime end);
+
     List<Booking> findAllByBooker_IdAndStatusOrderByStart(Long bookerId, BookingStatus status);
+
     List<Booking> findAllByItem_Id(Long itemId);
 }
--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-//    @Query("select i, b from Item i " +
-//            "left join Booking as b on b.item.id = i.id " +
-//            "where i.owner.id = ?1")
     List<Item> findAllByOwner_Id(Long id);
     @Query(value = "select * from items as i " +
             "where i.name ilike concat('%', ?1, '%') or " +
