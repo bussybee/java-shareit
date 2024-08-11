3ed19d770c383cc725f9abb18635229a768cc351
bussybee <katena.maslova.03@mail.ru>
2024-08-11 14:01:39 +0300

    fix

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOwner_Id(Long id);
+
     @Query(value = "select * from items as i " +
             "where i.name ilike concat('%', ?1, '%') or " +
             "i.description ilike concat('%', ?1, '%')", nativeQuery = true)
